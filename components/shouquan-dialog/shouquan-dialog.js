b79e6c8ec0ceb58694d4dd50e19e0ef3ca45052c
wangkaifeng <349210796@qq.com>
2021-04-20 10:33:26 +0800

    获取用户授权更改

--- components/shouquan-dialog/shouquan-dialog.js
+++ components/shouquan-dialog/shouquan-dialog.js
@@ -65,12 +65,19 @@ Component({
         wx.login({
           success(res) {
             console.log(res)
-            wx.getUserInfo({
-              success: function(ress) {
+            wx.getUserProfile({
+              desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+              success: (ress) => {
                 console.log(ress);
                 that.triggerEvent("success", { code:res.code,userInfo:ress });
               }
             })
+            // wx.getUserInfo({
+            //   success: function(ress) {
+            //     console.log(ress);
+            //     that.triggerEvent("success", { code:res.code,userInfo:ress });
+            //   }
+            // })
            
           }
         })
